dfd94176f1b8a8ec6238d84bca2d8bdfdda3bfa0
Enes Ajanovic <enes.ajanovic@zendev.se>
2025-01-21 23:03:56 +0100

    add back button

---
+++ src/app/favorite-products/components/GoBack.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import {useRouter} from 'next/navigation';
+import React from 'react';
+
+export const GoBack = () => {
+    const router = useRouter();
+
+    const handleGoBack = () => {
+        router.back();
+    };
+
+    return (
+        <button
+            aria-label={'Back to product list'}
+            onClick={handleGoBack}
+            className="px-4 mb-4 py-2 bg-blue-500 text-white rounded-md shadow hover:bg-blue-600 transition-colors"
+        >
+            Back to products
+        </button>
+    );
+};
+
